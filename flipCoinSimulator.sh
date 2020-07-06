7027ccf3a987a667873c2c5242fcb72090f275d3
RAJ <vermar4142@gmail.com>
2020-07-06 22:54:31 +0530

    Added:- Extended The uc3 to ensure if its a tie then game will continue with minimum of 2 pts diff

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -48,18 +48,18 @@ diffInCounts(){
 if [ $headCount -eq 21 ]
 then
 	winCount=$(($headCount-$tailCount))
-	echo "Head Wins With Count : "  $winCount "More From Tail Count"
+	echo "Head Wins With Count : "  $winCount " More From Tail Count"
 fi
 
 if [ $tailCount -eq 21 ]
 then
 	winCount=$(($tailCount-$headCount))
-	echo "Tail Wins With Count : " $winCount "More From Head Count"
+	echo "Tail Wins With Count : " $winCount " More From Head Count"
 fi
 
 if [ $headCount -eq 21 -a $tailCount -eq 21 ]
 then
-	echo "Both Wins With Tie Count of :  $tails tails and $head head "
+	echo "Both Wins With Tie Count of :  $tailCount Tails and $headCount Head "
 else
 	echo "There Was no Tie Between Head and Tails"
 fi
@@ -102,6 +102,76 @@ done
 
 echo "Head Won  "$headCount "Times"
 echo "Tail Won  "$tailCount "Times"
-printf "\n"
 diffInCounts
 printf "\n"
+
+echo "Extend UC 3 to ensure if its tie then the game continues till the difference of minimum 2 points is achieved"
+
+diffifGameTies() { 
+headCountDiff=0 
+tailCountDiff=0
+ 
+if [ $headCount -gt $tailCount ] 
+then 
+	headCountDiff=$(($headCount - $tailCount)) 
+elif [ $tailCount -gt $headCount ] 
+then 
+	tailCountDiff=$(($tailCount - $headCount))
+fi 
+
+if [ $headCountDiff -eq 1 -o $tailCountDiff -eq 1 ] 
+then 
+	runGameTillDiff 
+elif [ $headCountDiff -gt 1 ] 
+then 
+	echo "Difference Of Head Count is : "$headCountDiff " More Than Tails"
+	echo "Head Wins After a Tie : " $headCount 
+elif [ $tailCountDiff -gt 1 ] 
+then 
+	echo "Diffrence Of Tail Count is : "$tailCountDiff " More Than Heads"
+	echo "Tail Wins After a Tie : " $tailCount 
+fi
+}
+
+runGameTillDiff(){ 
+headCount=0 
+tailCount=0 
+while [ $headCount -lt 21 -a $tailCount -lt 21 ] 
+do 
+	flipCoin=$(( $RANDOM % 2 +1 )) 
+	if [ $flipCoin -eq 1 ] 
+	then 
+		headCount=$(($headCount+1)) 
+	fi 
+	
+	if [ $flipCoin -eq 2 ] 
+	then tailCount=$(($tailCount+1)) 
+	fi 
+done
+echo "After Running The Game Again "
+echo "Head Count is :  "$headCount "Times"
+echo "Tail Count is :  "$tailCount "Times" 
+diffifGameTies
+}
+
+headCount=0 
+tailCount=0 
+while [ $headCount -lt 21 -o $tailCount -lt 21 ] 
+do 
+	flipCoin=$(( $RANDOM % 2 +1 )) 
+	if [ $flipCoin -eq 1 -a $headCount -lt 21 ] 
+	then 
+		headCount=$(( $headCount+1 )) 
+	fi 
+
+	if [ $flipCoin -eq 2 -a $tailCount -lt 21 ] 
+	then 
+		tailCount=$(( $tailCount+1 )) 
+	fi
+done
+	 
+if [ "$tailCount" -eq "21" -a "$headCount" -eq "21" ] 
+then 
+	echo "Game Ties Between Head And Tail with Count : $tailCount tails and $headCount head "
+	runGameTillDiff
+fi
