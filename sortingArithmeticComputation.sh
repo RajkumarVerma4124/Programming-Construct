0c56ccceed102c81522328e5e47235768542dacb
RAJ <vermar4142@gmail.com>
2020-07-06 18:16:49 +0530

    Added:- Added the file for taking three inputs a b & c

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -3,3 +3,15 @@ echo "Welcome to the Arithmetic Computation & Sorting Program "
 printf "\n"
 echo "This problem computes different arithmetic expression and Sorts the results"
 printf "\n"
+
+echo "Writing program to take three inputs – a, b & c"
+printf "\n"
+read -p "Enter The First Number : " a
+echo "Your First number is : "$a
+printf "\n"
+read -p "Enter The Second Number : " b
+echo "Your Second number is : "$b
+printf "\n"
+read -p "Enter The Third Number : " c
+echo "Your Third number is : "$c
+printf "\n"
