ac1e27aa754109ca1c39f220ab6f7c5a3a006200
RAJ <vermar4142@gmail.com>
2020-07-06 19:46:05 +0530

    Added:- Added the simulation file to display head or tails as winners

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,3 +2,17 @@
 echo "Welcome To The Flip Coin Simulation Program"
 echo "This problem displays the winner Heads or Tails"
 printf "\n"
+
+echo "Simulating to start with Flipping a Coin to Display Heads or Tails as winner"
+flipCoin=$(( $RANDOM % 2 +1 ))
+echo $flipCoin    
+if [ $flipCoin -eq 1 ]
+then
+	smilulate=$flipCoin
+	echo "Heads Wins"
+else
+	smilulate=$flipCoin
+	echo "Tails Wins"
+fi
+printf "\n"
+
